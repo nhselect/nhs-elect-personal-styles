38ece072c7f13c076f6d53f1c909b93b5a7ce00f
timberrr <timberrr@users.noreply.github.com>
2023-05-18 15:15:15 +0100

    tki draft

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      { name: 'robots', content: 'noindex, nofollow' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
--- pages/tki/index.vue
+++ pages/tki/index.vue
@@ -3,23 +3,27 @@
     <div v-if="resultVisible" class="mr-result-container">
       <div class="nhsuk-width-container">
         <div class="nhsuk-grid-row">
-          <div class="nhsuk-grid-column-two-thirds">
+          <div class="nhsuk-grid-column-one-half">
             <div class="preferred-style-result">
-              <h1 class="nhsuk-heading-xl">Your preferred style:</h1>
+              <h1 class="nhsuk-heading-xl">
+                Your preferred style:
+              </h1>
               <div class="preferred-style-container">
-                <p>{{ preferredStyle }}</p>
+                <p class="nhsuk-u-font-size-64">
+                  {{ statements[preferredStyle] }}
+                </p>
               </div>
             </div>
           </div>
-          <div class="nhsuk-grid-column-one-third">
+          <div class="nhsuk-grid-column-one-half">
             <dl class="nhsuk-summary-list nhsuk-summary-list--no-border">
               <div
                 v-for="(result, option) in results"
                 :key="'result-' + option"
                 class="nhsuk-summary-list__row"
               >
-                <dt class="nhsuk-summary-list__key nhsuk-u-font-size-24">
-                  {{ option }}
+                <dt class="nhsuk-summary-list__key nhsuk-u-font-size-20">
+                  {{ statements[option] }} {{ result.count }}
                 </dt>
                 <dd class="nhsuk-summary-list__value">
                   <div
@@ -43,10 +47,9 @@
     </div>
     <div v-else>
       <h1>
-        Merrill & Reid Personal Style Inventory
+        Thomas Kilman Conflict Mode Instrument (TKI)
         <span class="nhsuk-caption-m">
-          Source: David Merrill & Roger Reid,
-          <em>Personal Styles and Effective Performance</em>, 1981
+          Source: Kenneth L. Thomas and Ralph H. Kilman
         </span>
       </h1>
       <p>
@@ -55,39 +58,41 @@
           collected or shared in any form
         </span>
       </p>
-      <p>Select the word or phrase in each set of 4 that is most like you:</p>
+      <p>Consider situations in which you find your wishes differing from those of another person. How do you usually respond to such situations?</p>
+      <p>Below are several pairs of statements describing possible behavioural responses. For each pair, select the statement which is most characteristic of your own behaviour.</p>
+      <p>In many cases, neither statement may be very typical of your behaviour, but please select the response which you would be more likely to use.</p>
       <ul class="nhsuk-grid-row nhsuk-card-group">
         <li
-          v-for="(set, index) in phraseSets.body"
-          :key="index"
-          class="nhsuk-grid-column-one-third nhsuk-card-group__item"
+          v-for="(set, index) in statementSets.sets"
+          :key="set.number"
+          class="nhsuk-grid-column-three-quarters nhsuk-card-group__item"
         >
           <div
             class="nhsuk-card"
-            :class="selectedPhrases[index] != null ? 'completed' : ''"
+            :class="selectedStatements[index] != null ? 'completed' : ''"
           >
             <div class="nhsuk-card__content">
               <div class="nhsuk-form-group">
                 <fieldset class="nhsuk-fieldset">
                   <div class="nhsuk-radios">
                     <div
-                      v-for="(phrase, option) in set"
+                      v-for="(statement, option) in set.options"
                       :key="set + '-' + option"
                       class="nhsuk-radios__item"
                     >
                       <input
-                        :id="'phrase-set-' + index + '-' + option"
-                        v-model="selectedPhrases[index]"
+                        :id="'statement-set-' + index + '-' + option"
+                        v-model="selectedStatements[index]"
                         class="nhsuk-radios__input"
-                        :name="'phrase-set-' + index + '-' + option"
+                        :name="'statement-set-' + index + '-' + option"
                         type="radio"
-                        :value="[option, phrase]"
+                        :value="[index, statement.text]"
                       />
                       <label
                         class="nhsuk-label nhsuk-radios__label"
-                        :for="'phrase-set-' + index + '-' + option"
+                        :for="'statement-set-' + index + '-' + option"
                       >
-                        {{ phrase }}
+                        {{ statement.text }}
                       </label>
                     </div>
                   </div>
@@ -102,10 +107,10 @@
           <div class="nhsuk-card">
             <div class="nhsuk-card__content">
               <p class="nhsuk-heading-l">
-                {{ selectedCount }}/{{ phraseCount }} selected
+                {{ selectedCount }}/{{ statementCount }} selected
               </p>
               <p v-if="!allSelected">
-                Make sure you've selected a phrase from every set then return
+                Make sure you've selected a statement from every set then return
                 here to see your results
               </p>
               <button
@@ -135,68 +140,61 @@
 <script>
 export default {
   async asyncData({ $content, error }) {
-    const phraseSets = await $content('merrill-reid/phrase-sets')
-      .only(['body'])
+    const statementSets = await $content('tki/statement-sets')
       .fetch()
       .catch((err) => {
         error({ statusCode: 404, message: err })
       })
 
-    const phraseCount = phraseSets.body.length
+    const statementCount = statementSets.sets.length
 
     return {
-      phraseSets,
-      phraseCount,
+      statementSets,
+      statementCount,
     }
   },
   data() {
     return {
-      selectedPhrases: [],
+      selectedStatements: [],
       resultVisible: false,
+      statements: {
+        'competing': 'Competing (forcing)',
+        'collaborating': 'Collaborating (problem solving)',
+        'compromising': 'Compromising (sharing)',
+        'avoiding': 'Avoiding (withdrawal)',
+        'accommodating': 'Accommodating (soothing)'
+      }
     }
   },
   head: {
-    title: 'Merrill & Reid Personal Style Inventory',
+    title: 'Thomas Kilman Conflict Mode Instrument (TKI)',
   },
   computed: {
     selectedCount() {
-      return this.selectedPhrases.filter((p) => p !== null).length
+      return this.selectedStatements.filter((p) => p !== null).length
     },
     allSelected() {
-      const completed = this.selectedCount === this.phraseCount
+      const completed = this.selectedCount === this.statementCount
       return completed
     },
     results() {
-      const options = ['A', 'B', 'C', 'D']
-      const weighting = {
-        A: [
-          11, 16, 23, 32, 38, 46, 54, 62, 67, 72, 77, 85, 92, 96, 99, 103, 106,
-          110, 113,
-        ],
-        B: [
-          4, 17, 21, 35, 43, 51, 62, 71, 76, 81, 86, 94, 101, 105, 108, 111,
-          115,
-        ],
-        C: [
-          15, 22, 38, 45, 49, 64, 68, 73, 81, 86, 92, 96, 99, 103, 107, 110,
-          114,
-        ],
-        D: [11, 24, 32, 41, 47, 67, 70, 79, 86, 93, 99, 103, 106, 110, 114],
-      }
+      const options = [
+        'competing', 'collaborating', 'compromising', 'avoiding', 'accommodating'
+      ]
       const selectedCounts = {
-        A: { count: 0, barLength: 0 },
-        B: { count: 0, barLength: 0 },
-        C: { count: 0, barLength: 0 },
-        D: { count: 0, barLength: 0 },
+        competing: { count: 0, barLength: 0 },
+        collaborating: { count: 0, barLength: 0 },
+        compromising: { count: 0, barLength: 0 },
+        avoiding: { count: 0, barLength: 0 },
+        accommodating: { count: 0, barLength: 0 },
       }
-      for (let i = 0; i < options.length; i++) {
-        const count = this.selectedPhrases.filter(
-          (p) => p[0] === options[i]
-        ).length
-        const maxPossible = weighting[options[i]][weighting[options[i]].length-1]
-        const barLength = weighting[options[i]][count] ?? maxPossible
-        selectedCounts[options[i]].count = count
-        selectedCounts[options[i]].barLength = barLength
+      for (let i = 0; i < this.selectedStatements.length; i++) {
+        const qn = this.statementSets.sets[this.selectedStatements[i][0]]
+        const ans = this.selectedStatements[i][1]
+        const char = qn.options.find(o => o.text === ans).behaviour
+
+        selectedCounts[char].count += 1
+        selectedCounts[char].barLength += 1
       }
       return selectedCounts
     },
@@ -215,7 +213,7 @@ export default {
   },
   methods: {
     clearSelection() {
-      this.selectedPhrases = []
+      this.selectedStatements = []
       this.resultVisible = false
     },
     viewResult() {
@@ -260,7 +258,6 @@ export default {
     margin-bottom: 32px;
 
     p {
-      font-size: 200px;
       vertical-align: top;
       font-weight: $nhsuk-font-bold;
       color: $color_nhsuk-white;
